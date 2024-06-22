96766362a08f9bbf5c66541a50f28289692f3933
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-06-22 10:19:47 +0200

    Added Framework

---
+++ localFramework/localFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  localFramework
+//
+//  Created by Manyuchi, Carrington C on 2024/06/22.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "I did something boy"
+    }
+    
+}
---
+++ localFramework/localFramework/localFramework.h
@@ -0,0 +1,18 @@
+//
+//  localFramework.h
+//  localFramework
+//
+//  Created by Manyuchi, Carrington C on 2024/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for localFramework.
+FOUNDATION_EXPORT double localFrameworkVersionNumber;
+
+//! Project version string for localFramework.
+FOUNDATION_EXPORT const unsigned char localFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <localFramework/PublicHeader.h>
+
+
